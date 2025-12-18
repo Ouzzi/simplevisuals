55cbf5dd5aedfb53ef5c9bd28928619c515c3dea
Oussama Azarfane <62378600+Ouzzi@users.noreply.github.com>
2025-12-18 04:33:56 +0100

    todo.md

--- src/main/java/com/simplevisuals/Simplevisuals.java
+++ src/main/java/com/simplevisuals/Simplevisuals.java
@@ -1,5 +1,6 @@
 package com.simplevisuals;
 
+import com.simplevisuals.command.ModCommands;
 import com.simplevisuals.config.SimplevisualsConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
@@ -12,21 +13,19 @@ public class Simplevisuals implements ModInitializer {
 	public static final String MOD_ID = "simplevisuals";
     private static SimplevisualsConfig CONFIG;
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
         LOGGER.info("Initializing Simplevisuals mod...");
 
         AutoConfig.register(SimplevisualsConfig.class, GsonConfigSerializer::new);
         CONFIG = AutoConfig.getConfigHolder(SimplevisualsConfig.class).getConfig();
-	}
+
+        ModCommands.register();
+
+    }
 
     public static SimplevisualsConfig getConfig() { return CONFIG; }
 }
+
---
+++ todo.md
@@ -0,0 +1,6 @@
+## todo: implement
+- chat death message - show who killed who, how and where
+
+
+## BUGS:
+- sometimes pickup notifier doesnt show picked up item (sometimes multiple items dont show)
